7bb7db2791b24d744038d5ec88db95ee943d1bab
Aprilian Ihza <ihzaaprilian@gmail.com>
2022-10-12 09:42:49 +0700

    update search

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let listMovie = document.getElementById("list")
+let listSearch = document.getElementById("list-search")
 let inputSearch = document.getElementById("search")
 let button = document.getElementById("submit")
 let body = document.getElementsByTagName("body")
@@ -13,7 +14,7 @@ let getDataMovie = async (URL) => {
     movies.results.forEach((item, index) => {
         let IMG = 'https://image.tmdb.org/t/p/w500'
         listMovie.innerHTML += 
-        `<div class="h-64 w-64 ml-4 p-10 rounded-lg mt-8 border border-gray-200">
+        `<div class="shadow-lg h-64 w-64 ml-4 p-10 rounded-lg mt-8 border border-gray-200">
         <img src="${IMG+item.poster_path}" class="rounded-lg" width="150" alt="">
         <h1 class="mt-2 font-semibold">${item.title}</h1>
         <h1 class="mt-2">Realese : ${item.release_date}</h1>
@@ -21,6 +22,25 @@ let getDataMovie = async (URL) => {
         </div>`
     })
   }
+
+  let getDataSearch = async (URL) => {
+    let response = await fetch(URL)
+    let movies = await response.json()
+    console.log(movies.results);
+
+    movies.results.forEach((item, index) => {
+        let IMG = 'https://image.tmdb.org/t/p/w500'
+        listMovie.remove()
+        listSearch.innerHTML += 
+        `<div class="shadow-lg h-64 w-64 ml-4 p-10 rounded-lg mt-8 border border-gray-200">
+        <img src="${IMG+item.poster_path}" class="rounded-lg" width="150" alt="">
+        <h1 class="mt-2 font-semibold">${item.title}</h1>
+        <h1 class="mt-2">Realese : ${item.release_date}</h1>
+        <h1 class="mt-2">Rating : ${item.vote_average}</h1>
+        </div>`
+    })
+  }
+
   getDataMovie(API_URL)
 
   form.addEventListener('submit', (e) => {
@@ -29,7 +49,7 @@ let getDataMovie = async (URL) => {
     e.preventDefault()
 
     if(searchValue && searchValue !==''){
-        getDataMovie(SEARCH_URL+searchValue+'&page=1')
+        getDataSearch(SEARCH_URL+searchValue+'&page=1')
         searchValue = ''
     }else{
         window.location.reload()
