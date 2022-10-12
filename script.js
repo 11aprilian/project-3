a1a286ac339305e87975c922633a5e42ecfe5816
Aprilian Ihza <ihzaaprilian@gmail.com>
2022-10-12 13:47:15 +0700

    fix search bug

--- script.js
+++ script.js
@@ -2,18 +2,16 @@ let listMovie = document.getElementById("list")
 let listSearch = document.getElementById("list-search")
 let inputSearch = document.getElementById("search")
 let button = document.getElementById("submit")
-let body = document.getElementsByTagName("body")
 let form = document.getElementById("form");
 let API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=d41688cb0f0ee69b62145a634b120198&language=en-US"
 
 let getDataMovie = async (URL) => {
     let response = await fetch(URL)
     let movies = await response.json()
-    console.log(movies.results);
 
     movies.results.forEach((item, index) => {
         let IMG = 'https://image.tmdb.org/t/p/w500'
-        listMovie.innerHTML += 
+        listMovie.innerHTML +=
         `<div class="shadow-lg h-64 w-64 ml-4 p-10 rounded-lg mt-8 border border-gray-200">
         <img src="${IMG+item.poster_path}" class="rounded-lg" width="150" alt="">
         <h1 class="mt-2 font-semibold">${item.title}</h1>
@@ -23,22 +21,28 @@ let getDataMovie = async (URL) => {
     })
   }
 
-  let getDataSearch = async (URL) => {
+
+let getDataSearch = async (URL) => {
     let response = await fetch(URL)
     let movies = await response.json()
-    console.log(movies.results);
+    let html = '';
 
     movies.results.forEach((item, index) => {
         let IMG = 'https://image.tmdb.org/t/p/w500'
         listMovie.remove()
-        listSearch.innerHTML += 
+        let htmlSegment =
         `<div class="shadow-lg h-64 w-64 ml-4 p-10 rounded-lg mt-8 border border-gray-200">
         <img src="${IMG+item.poster_path}" class="rounded-lg" width="150" alt="">
         <h1 class="mt-2 font-semibold">${item.title}</h1>
         <h1 class="mt-2">Realese : ${item.release_date}</h1>
         <h1 class="mt-2">Rating : ${item.vote_average}</h1>
         </div>`
+
+        html += htmlSegment;
     })
+
+    let container = document.querySelector('.container')
+    container.innerHTML = html
   }
 
   getDataMovie(API_URL)
@@ -47,13 +51,6 @@ let getDataMovie = async (URL) => {
     let SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=d41688cb0f0ee69b62145a634b120198&query='
     let searchValue = inputSearch.value
     e.preventDefault()
+      getDataSearch(SEARCH_URL+searchValue+'&page=1')
 
-    if(searchValue && searchValue !==''){
-        getDataSearch(SEARCH_URL+searchValue+'&page=1')
-        searchValue = ''
-    }else{
-        window.location.reload()
-    }
-    
   })
-
